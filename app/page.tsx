0acab3a7314c5900620eab13f556000bbdc54057
gk6294 <bartonchik05@gmail.com>
2024-10-07 18:10:55 +0800

    ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+"use client";
 import { Button } from "@/components/ui/button";
-
+import { useRouter } from "next/navigation";
 export default function Home() {
+  const router = useRouter();
   return (
     <div className="">
-      <Button>Hello World</Button>
+      <Button onClick={() => router.push("/authenticate")}>Try it Now!</Button>
     </div>
   );
 }
