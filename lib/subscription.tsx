cc9d9cd6779b89e608ad8fba94ae57f11b37397c
gk6294 <bartonchik05@gmail.com>
2024-10-18 20:28:52 +0800

    add !!isvalid to avoid return null

--- lib/subscription.tsx
+++ lib/subscription.tsx
@@ -27,5 +27,5 @@ export const checkSubscription = async () => {
     userSubscription.stripeCurrentPeriodEnd &&
     userSubscription.stripeCurrentPeriodEnd.getTime() + DAY_IN_MS > Date.now();
 
-  return isValid;
+  return !!isValid;
 };
